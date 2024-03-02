00a71dc6139931ecc6c66f89ba0fab5031fcc3d3
Yoko0202 <136854921+Yoko0202@users.noreply.github.com>
2024-03-02 11:53:07 -0500

    Add files via upload

---
+++ R00_IDS03_J.java
@@ -0,0 +1,14 @@
+package main;
+
+public class R00_IDS03_J {
+
+	public R00_IDS03_J() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
