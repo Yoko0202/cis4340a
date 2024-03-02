dac9c9b5ccd548a621bd3662c66a8e76a1208e1f
Yoko0202 <136854921+Yoko0202@users.noreply.github.com>
2024-03-02 12:06:15 -0500

    Add files via upload

---
+++ R02_XP00_J.java
@@ -0,0 +1,14 @@
+package main;
+
+public class R02_XP00_J {
+
+	public R02_XP00_J() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
