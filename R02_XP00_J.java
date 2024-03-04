e395d481af707e0edc303eb1b34d38f2bdaefd23
Yoko0202 <136854921+Yoko0202@users.noreply.github.com>
2024-03-03 19:56:07 -0500

    Update R02_XP00_J.java

--- R02_XP00_J.java
+++ R02_XP00_J.java
@@ -1,72 +1,19 @@
 package main;
 
-import java.util.logging.Logger;
+import java.io.File;
 
-class SanitizedTextLogger extends Logger {
-    Logger delegate;
+public class R02_XP00_J {
 
-    public SanitizedTextLogger(Logger delegate) {
-        super(delegate.getName(), delegate.getResourceBundleName());
-        this.delegate = delegate;
-    }
-
-    public String sanitize(String msg) {
-        String[] lines = msg.split("\n");
-        StringBuilder sanitizedMsg = new StringBuilder(lines[0]);
-        for (int i = 1; i < lines.length; i++) {
-            sanitizedMsg.append("\n  ").append(lines[i]);
-        }
-        return sanitizedMsg.toString();
-    }
-
-    @Override
-    public void severe(String msg) {
-        delegate.severe(sanitize(msg));
-    }
-
-    @Override
-    public void warning(String msg) {
-        delegate.warning(sanitize(msg));
-    }
-
-    @Override
-    public void info(String msg) {
-        delegate.info(sanitize(msg));
-    }
-
-    @Override
-    public void config(String msg) {
-        delegate.config(sanitize(msg));
-    }
-
-    @Override
-    public void fine(String msg) {
-        delegate.fine(sanitize(msg));
-    }
-
-    @Override
-    public void finer(String msg) {
-        delegate.finer(sanitize(msg));
-    }
-
-    @Override
-    public void finest(String msg) {
-        delegate.finest(sanitize(msg));
-    }
-}
-
-public class R00_IDS03_J {
     public static void main(String[] args) {
-        Logger logger = Logger.getLogger("TestLogger");
-        SanitizedTextLogger sanLogger = new SanitizedTextLogger(logger);
+        deleteFile();
+    }
 
-        boolean loginSuccessful = true;
-        String username = "User";
-
-        if (loginSuccessful) {
-            sanLogger.severe("User login succeeded for: " + username);
-        } else {
-            sanLogger.severe("User login failed for: " + username);
+    public static void deleteFile() {
+        File someFile = new File("someFileName.txt");
+        // Do something with someFile
+        if (!someFile.delete()) {
+            // Handle failure to delete the file
+            System.err.println("Failed to delete the file.");
         }
     }
 }
