0effb20666225ebdb0da5c981f1c6430dd5f0a8e
Yoko0202 <136854921+Yoko0202@users.noreply.github.com>
2024-03-03 19:55:02 -0500

    Update R02_XP00_J.java

--- R02_XP00_J.java
+++ R02_XP00_J.java
@@ -1,14 +1,72 @@
 package main;
 
-public class R02_XP00_J {
+import java.util.logging.Logger;
 
-	public R02_XP00_J() {
-		// TODO Auto-generated constructor stub
-	}
+class SanitizedTextLogger extends Logger {
+    Logger delegate;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    public SanitizedTextLogger(Logger delegate) {
+        super(delegate.getName(), delegate.getResourceBundleName());
+        this.delegate = delegate;
+    }
 
-	}
+    public String sanitize(String msg) {
+        String[] lines = msg.split("\n");
+        StringBuilder sanitizedMsg = new StringBuilder(lines[0]);
+        for (int i = 1; i < lines.length; i++) {
+            sanitizedMsg.append("\n  ").append(lines[i]);
+        }
+        return sanitizedMsg.toString();
+    }
 
+    @Override
+    public void severe(String msg) {
+        delegate.severe(sanitize(msg));
+    }
+
+    @Override
+    public void warning(String msg) {
+        delegate.warning(sanitize(msg));
+    }
+
+    @Override
+    public void info(String msg) {
+        delegate.info(sanitize(msg));
+    }
+
+    @Override
+    public void config(String msg) {
+        delegate.config(sanitize(msg));
+    }
+
+    @Override
+    public void fine(String msg) {
+        delegate.fine(sanitize(msg));
+    }
+
+    @Override
+    public void finer(String msg) {
+        delegate.finer(sanitize(msg));
+    }
+
+    @Override
+    public void finest(String msg) {
+        delegate.finest(sanitize(msg));
+    }
+}
+
+public class R00_IDS03_J {
+    public static void main(String[] args) {
+        Logger logger = Logger.getLogger("TestLogger");
+        SanitizedTextLogger sanLogger = new SanitizedTextLogger(logger);
+
+        boolean loginSuccessful = true;
+        String username = "User";
+
+        if (loginSuccessful) {
+            sanLogger.severe("User login succeeded for: " + username);
+        } else {
+            sanLogger.severe("User login failed for: " + username);
+        }
+    }
 }
